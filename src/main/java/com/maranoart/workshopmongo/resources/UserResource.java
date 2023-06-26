035bbd8ccd94e1775778ad3b134be03109fae2d8
MaranoMMV <matheusmmv14@gmail.com>
2023-06-26 17:06:47 -0300

    Delecao de usuario com requisicao de delete

--- src/main/java/com/maranoart/workshopmongo/resources/UserResource.java
+++ src/main/java/com/maranoart/workshopmongo/resources/UserResource.java
@@ -33,9 +33,7 @@ public class UserResource {
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){
-		
 		User obj = service.findById(id);
-		
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 	
@@ -45,6 +43,12 @@ public class UserResource {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
