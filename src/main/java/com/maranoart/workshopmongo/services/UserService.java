29e1e9c04347e5645d35f6137bf2dbcba41c665f
MaranoMMV <matheusmmv14@gmail.com>
2023-06-26 17:22:42 -0300

    Atualizacao de usuario com a requisicao put

--- src/main/java/com/maranoart/workshopmongo/services/UserService.java
+++ src/main/java/com/maranoart/workshopmongo/services/UserService.java
@@ -35,6 +35,16 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
