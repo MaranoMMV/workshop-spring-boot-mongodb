72c41982c5e968e505172f151fa7b6fd293cd63d
MaranoMMV <matheusmmv14@gmail.com>
2023-06-27 16:52:01 -0300

    Acrescentando comentarios aos posts! !

--- src/main/java/com/maranoart/workshopmongo/domain/Post.java
+++ src/main/java/com/maranoart/workshopmongo/domain/Post.java
@@ -2,12 +2,15 @@ package com.maranoart.workshopmongo.domain;
 
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.maranoart.workshopmongo.dto.AuthorDTO;
+import com.maranoart.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String  body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -70,6 +75,14 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -87,6 +100,8 @@ public class Post implements Serializable {
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 
 	
 	
