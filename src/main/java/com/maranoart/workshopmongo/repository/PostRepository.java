f90f059e4a92b079d8cc83cd427633f2d67c8f3e
MaranoMMV <matheusmmv14@gmail.com>
2023-06-27 17:33:02 -0300

    Consulta simples com query methods

--- src/main/java/com/maranoart/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/maranoart/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.maranoart.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +10,7 @@ import com.maranoart.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
-	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 	
 }
