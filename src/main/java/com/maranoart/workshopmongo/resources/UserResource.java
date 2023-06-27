8c8ef90f3fffb920d26d63c21aea631820900335
MaranoMMV <matheusmmv14@gmail.com>
2023-06-27 16:28:25 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/maranoart/workshopmongo/resources/UserResource.java
+++ src/main/java/com/maranoart/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.maranoart.workshopmongo.domain.Post;
 import com.maranoart.workshopmongo.domain.User;
 import com.maranoart.workshopmongo.dto.UserDTO;
 import com.maranoart.workshopmongo.services.UserService;
@@ -30,6 +31,8 @@ public class UserResource {
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){
@@ -59,4 +62,11 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPost(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
 }
