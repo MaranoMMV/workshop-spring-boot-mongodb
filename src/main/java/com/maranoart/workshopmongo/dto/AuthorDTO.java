3e20a734ecf1193efaca2fa83a6c28b8fa5bd3b9
MaranoMMV <matheusmmv14@gmail.com>
2023-06-27 16:15:28 -0300

    Projecao dos dados do author com DTO

---
+++ src/main/java/com/maranoart/workshopmongo/dto/AuthorDTO.java
@@ -0,0 +1,51 @@
+package com.maranoart.workshopmongo.dto;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.maranoart.workshopmongo.domain.User;
+
+public class AuthorDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	
+	public AuthorDTO() {
+	}
+	public AuthorDTO(User obj) {
+		this.id = obj.getId();
+		this.name = obj.getName();
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AuthorDTO other = (AuthorDTO) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
